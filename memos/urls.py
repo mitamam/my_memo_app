31fa362e4831d74383761eeec9a83e39f32df97b
mitamam <mitama.md@gmail.com>
2023-05-18 21:17:25 +0900

    fix: Fix incorrect view function names in urls.py

--- memos/urls.py
+++ memos/urls.py
@@ -5,11 +5,11 @@ from . import views
 app_name = 'memos'
 urlpatterns = [
     # ex. /memos/
-    path('', views.index, name='index'),
+    path('', views.memo_list_view, name='memo-list'),
     # ex. /memos/create/
-    path('create/', views.create, name='create'),
+    path('create/', views.memo_create_view, name='memo-create'),
     # ex. /memos/{memo_id}/edit
-    path('<int:memo_id>/edit/', views.edit, name='edit'),
+    path('<int:memo_id>/edit/', views.memo_edit_view, name='memo-edit'),
     # ex. /memos/{memo_id}/delete/
-    path('<int:memo_id>/delete/', views.delete, name='delete'),
+    path('<int:memo_id>/delete/', views.memo_delete_view, name='memo-delete'),
 ]
