1441fcd4f4d60eecf5ad62060f3895c429f3ba7e
mitamam <mitama.md@gmail.com>
2023-05-18 20:50:37 +0900

    feat: Add initial models using Django's ORM

--- memos/models.py
+++ memos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Memo(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=300)
+    created_at = models.DateTimeField()
