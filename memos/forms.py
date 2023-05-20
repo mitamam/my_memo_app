9a1c2aff1ea7ed91d0a6952589616d7efde1f024
mitamam <mitama.md@gmail.com>
2023-05-20 15:13:30 +0900

    feat: Add the feature to edit memos

---
+++ memos/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class MemoForm(forms.Form):
+    title = forms.CharField(label="Title: ", max_length=100)
+    content = forms.CharField(label="Content: ", widget=forms.Textarea, max_length=300)
+    created_at = forms.DateTimeField(label="Created at: ")
+
--- memos/views.py
+++ memos/views.py
@@ -1,6 +1,9 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 from .models import Memo
+from .forms import MemoForm
 
 
 def memo_list_view(request):
@@ -10,13 +13,32 @@ def memo_list_view(request):
 
 
 def memo_create_view(request):
-    return render(request, "memos/memo_form.html")
+    context = {"memo": None}
+    return render(request, "memos/memo_form.html", context)
 
 
 def memo_edit_view(request, memo_id):
-    memo = get_object_or_404(Memo, pk=memo_id)
-    context = {"memo": memo}
-    return render(request, "memos/memo_form.html", context)
+    if request.method == "POST":
+        form = MemoForm(request.POST)
+        memo = get_object_or_404(Memo, pk=memo_id)
+        if form.is_valid():
+            memo.title = form.cleaned_data["title"]
+            memo.content = form.cleaned_data["content"]
+            memo.created_at = form.cleaned_data["created_at"]
+            memo.save()
+            return HttpResponseRedirect(reverse("memos:memo-list"))
+        else:
+            return render(
+                request,
+                "memos/memo_form.html",
+                {"memo": memo, "error_message:": "Incorrect Input!"},
+            )
+    else:
+        memo = get_object_or_404(Memo, pk=memo_id)
+        initial_data = {'title': memo.title, 'content': memo.content, 'created_at': memo.created_at}
+        form = MemoForm(initial=initial_data)
+        context = {"memo": memo, "form": form}
+        return render(request, "memos/memo_form.html", context)
 
 
 def memo_delete_view(request, memo_id):
