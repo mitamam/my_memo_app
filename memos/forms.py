7c19020ab790bebdd075d9cb5d26ca44e06c4879
mitamam <mitama.md@gmail.com>
2023-05-21 23:40:36 +0900

    feat: Add classes and change datetime format to fields in forms.py

--- memos/forms.py
+++ memos/forms.py
@@ -2,6 +2,17 @@ from django import forms
 
 
 class MemoForm(forms.Form):
-    title = forms.CharField(label="Title", max_length=100)
-    content = forms.CharField(label="Content", widget=forms.Textarea, max_length=300)
-    created_at = forms.DateTimeField(label="Created at", widget=forms.SelectDateWidget)
+    title = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    content = forms.CharField(
+        max_length=300,
+        widget=forms.Textarea(attrs={'class': 'form-control'})
+    )
+    created_at = forms.DateTimeField(
+        widget=forms.DateTimeInput(
+            attrs={'class': 'form-control mb-2'},
+            format="%Y-%m-%d %H:%M"
+        )
+    )
