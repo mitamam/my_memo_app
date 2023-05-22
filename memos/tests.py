c82ecea72908a82dd46374e68b3201324860fc2d
mitamam <mitama.md@gmail.com>
2023-05-22 22:43:41 +0900

    test: Add test for memo_create_view

--- memos/tests.py
+++ memos/tests.py
@@ -24,3 +24,46 @@ class MemoListTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "test memo")
         self.assertQuerysetEqual(response.context["memo_list"], [test_memo])
+
+
+class MemoCreateTests(TestCase):
+    def test_empty_input_field(self):
+        data = {
+            "title": "",
+            "content": "This is the test memo.",
+            "created_at": timezone.now()
+        }
+        response = self.client.post(reverse("memos:memo-create"), data)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "正しい値を入力してください")
+
+    def test_incorrect_date_time(self):
+        data = {
+            "title": "test memo",
+            "content": "This is the test memo.",
+            "created_at": "Incorrect date"
+        }
+        response = self.client.post(reverse("memos:memo-create"), data)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "正しい値を入力してください")
+
+    def test_title_is_more_than_max_length(self):
+        data = {
+            # 101 characters
+            "title": "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabc"\
+            "defghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvw",
+            "content": "This is the test memo.",
+            "created_at": timezone.now()
+        }
+        response = self.client.post(reverse("memos:memo-create"), data)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "正しい値を入力してください")
+
+    def test_correct_values(self):
+        data = {
+            "title": "test memo",
+            "content": "This is the test memo.",
+            "created_at": timezone.now()
+        }
+        response = self.client.post(reverse("memos:memo-create"), data)
+        self.assertEqual(response.status_code, 302)
